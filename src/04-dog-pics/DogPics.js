266c3f426dbe079494147dc1e4352f95736a5655
eggzylog <109510137+eggzylog@users.noreply.github.com>
2022-11-28 13:48:12 +0000

    01_04 done

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react"
+
 export default function DogPics () {
   // API: https://dog.ceo/dog-api/
+  const [dogUrl, setDogUrl] = useState('');
+
+  const getDogUrl = async () => {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await response.json();
+    return data.message;
+  }
+
+  const handleClick = () => {
+    getDogUrl().then(res => setDogUrl(res));
+  }
+
+  useEffect(() => {
+    getDogUrl().then(res => setDogUrl(res))
+  }, []);
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={dogUrl} alt="Dog" />
+      <button onClick={handleClick}>🐶</button>
     </div>
   )
 }
